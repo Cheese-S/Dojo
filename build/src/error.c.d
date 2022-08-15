6c9c085c56f6177b0f998a5776e63ff313ea5925
Cheese_S <32346161+Cheese-S@users.noreply.github.com>
2022-08-15 00:35:31 -0400

    working?

--- build/src/error.c.d
+++
@@ -1,5 +0,0 @@
-build/./src/error.c.o: src/error.c src/error.h src/scanner.h
-
-src/error.h:
-
-src/scanner.h:
--- build/tests/check_scanner.c.d
+++
@@ -1,4 +0,0 @@
-build/./tests/check_scanner.c.o: tests/check_scanner.c \
- tests/../src/scanner.h
-
-tests/../src/scanner.h:
